6a22d4f585093e859a8f91be1cffa7083082f8b1
Kevin Huang <yellow.khaos@gmail.com>
2018-10-09 18:40:29 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -29,7 +29,7 @@ class Customer {
     store.customers.push(this)
   }
   deliveries() {
-
+    return store.deliveries.filter(function(delivery){return delivery.customerId === this.id}.bind(this))
   }
   meals() {
 
