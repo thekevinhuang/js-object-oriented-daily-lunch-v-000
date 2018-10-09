b27f826deb84a3504c0a5e4c571cf28a6c458b5b
Kevin Huang <yellow.khaos@gmail.com>
2018-10-09 17:52:12 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,2 +1,72 @@
 // global datastore
 let store = { neighborhoods: [], meals: [], customers: [], deliveries: [] };
+let neighborhoodId = 0
+let customerId = 0
+let mealId = 0
+let deliveryId = 0
+
+class Neighborhood {
+  constructor (name){
+    this.name = name
+    this.id = ++neighborhoodId
+    store.neighborhoods.push(this)
+  }
+
+  deliveries() {
+    
+  }
+  customers() {
+    
+  }
+}
+
+class Customer {
+  constructor (name, neighborhoodId){
+    this.name = name
+    this.neighborhoodId = neighborhoodId
+    this.id = ++customerId
+    store.customers.push(this)
+  }
+  deliveries() {
+    
+  }
+  meals() {
+    
+  }
+
+}
+
+class Meal {
+  constructor (title, price){
+    this.title = title
+    this.price = price
+    this.id = ++mealId
+    store.meals.push(this)
+  }
+  deliveries(){
+    
+  }
+
+  customers(){
+    
+  }
+}
+
+class Delivery {
+  constructor (mealId, neighborhoodId, customerId){
+    this.mealId = mealId
+    this.neighborhoodId = neighborhoodId
+    this.customerId = customerId
+    this.id = ++deliveryId
+    store.deliveries.push(this)
+  }
+  meal(){
+    
+  }
+  customer(){
+    
+  }
+  neighborhood(){
+    
+  }
+}
